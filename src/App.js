174862dc6e5812d5f20d4a37033b5cdcf7dfcbd0
Haider <headeralishah@gmail.com>
2017-02-19 13:04:15 +0500

    delete improved.

--- src/App.js
+++ src/App.js
@@ -15,47 +15,41 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.deleteTodo = this.deleteTodo.bind(this);
+    // this.deleteTodo = this.deleteTodo.bind(this);
     this.addTodo = this.addTodo.bind(this);
     this.state = { todos: [] };
-    fb.database().ref('/todos').on('child_added', (data) => {
-      let obj = data.val();
-      obj.id = data.key;
-      let currentTodos = this.state.todos;
-      currentTodos.push(obj);
-      this.setState({ todos: currentTodos })
-      console.log(this.state.todos, 'obj');
-    })
+    // fb.database().ref('/todos').on('child_added', (data) => {
+    //   let obj = data.val();
+    //   obj.id = data.key;
+    //   let currentTodos = this.state.todos;
+    //   currentTodos.push(obj);
+    //   this.setState({ todos: currentTodos })
+    //   console.log(this.state.todos, 'obj');
+    // })
   }
 
   addTodo(ev) {
     ev.preventDefault();
-    console.log(this.refs.todo.value);
     fb.database().ref('todos/').push({ todo: this.refs.todo.value })
       .then((v) => {
 
       })
     this.refs.todo.value = '';
   }
-  deleteTodo(todoKey) {
-    console.log(todoKey, '******************');
-    fb.database().ref('todos/' + todoKey).remove()
-      .then((v) => {
-        console.log(this.state.todos, 'deleted');
-        let currentTodo = this.state.todos;
-        let indexRemove;
-        for (var i = 0; i < currentTodo.length; i++) {
-          if (currentTodo[i].id === todoKey) {
-            indexRemove = i;
-          }
-        }
-        console.log(indexRemove, 'indexRemove');
-        currentTodo = currentTodo.slice(0, indexRemove).concat(currentTodo.slice(indexRemove+1));
-        console.log(currentTodo, 'currentTodo');
-        this.setState({ todos: currentTodo });
-        console.log(this.state.todos, '55555555555555');
-      });
-  }
+  // deleteTodo(todoKey) {
+  //   fb.database().ref('todos/' + todoKey).remove()
+  //     .then((v) => {
+  //       let currentTodo = this.state.todos;
+  //       let indexRemove;
+  //       for (var i = 0; i < currentTodo.length; i++) {
+  //         if (currentTodo[i].id === todoKey) {
+  //           indexRemove = i;
+  //         }
+  //       }
+  //       currentTodo = currentTodo.slice(0, indexRemove).concat(currentTodo.slice(indexRemove+1));
+  //       this.setState({ todos: currentTodo });
+  //     });
+  // }
   render() {
     return (
       <div className="App">
@@ -69,7 +63,7 @@ class App extends Component {
           <button>Add Todo</button>
         </form>
         <br /><br />
-        <Todo todos={this.state.todos} deleteHandler={this.deleteTodo}></Todo>
+        <Todo ></Todo>
       </div>
     );
   }
--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import * as firebase from 'firebase';
+import * as firebase from 'firebase';
 // var fb = firebase.initializeApp({
 //     apiKey: "AIzaSyAUTWn2F8u3Q7oeYrYHp8OwsDajPk_RB-A",
 //     authDomain: "test-01-141907.firebaseapp.com",
@@ -14,23 +14,55 @@ class Todo extends Component {
         super(props);
         console.log(this.props.todos, 'haider');
         this.deleteTodoHandler = this.deleteTodoHandler.bind(this);
+        this.editTodoHandler = this.editTodoHandler.bind(this);
+        this.state = { todos: [] };
+        firebase.database().ref('/todos').on('child_added', (data) => {
+            let obj = data.val();
+            obj.id = data.key;
+            let currentTodos = this.state.todos;
+            currentTodos.push(obj);
+            this.setState({ todos: currentTodos })
+            console.log(this.state.todos, 'obj');
+        })
     }
 
     deleteTodoHandler(ev) {
-        console.log(ev.target.value);
-        return this.props.deleteHandler(ev.target.value);
+        // console.log(ev.target.value);
+        // return this.props.deleteHandler(ev.target.value);
+        let todoKey = ev.target.dataset.id;
+        firebase.database().ref('todos/' + todoKey).remove()
+            .then((v) => {
+                let currentTodo = this.state.todos;
+                let indexRemove;
+                for (var i = 0; i < currentTodo.length; i++) {
+                    if (currentTodo[i].id === todoKey) {
+                        indexRemove = i;
+                    }
+                }
+                currentTodo = currentTodo.slice(0, indexRemove).concat(currentTodo.slice(indexRemove + 1));
+                this.setState({ todos: currentTodo });
+            });
+    }
+
+    editTodoHandler(ev) {
+        console.log(ev.target.dataset.id, '*********');
+        console.log(ev.target.dataset.todo, '*********');
     }
 
     render() {
         return (
             <div>
-                {this.props.todos.map((v, i) => {
+                {this.state.todos.map((v, i) => {
                     return (
-                        <h1 key={i}>{v.todo} <button value={v.id} onClick={this.deleteTodoHandler}>Delete</button></h1>
+                        <h1 key={i}>{v.todo} <button data-id={v.id} onClick={this.deleteTodoHandler}>Delete</button>  <button data-id={v.id} data-todo={v.todo} onClick={this.editTodoHandler}>Edit</button></h1>
 
                     )
                 })
                 }
+                <br />   <br />
+
+                {(this.state.todos.length > 0) ? <h2>Hello World Bindaaaas</h2> : this.props.children}
+
             </div>
         )
 
